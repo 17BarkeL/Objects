fd96a035901d56341dc48fc6a80fee375a5fe6e8
Luke Barkess <luke.barkess@icloud.com>
2022-09-21 10:19:04 +0100

    objects

--- Objects/Program.cs
+++ Objects/Program.cs
@@ -10,6 +10,34 @@ namespace Objects
     {
         static void Main(string[] args)
         {
+            Student luke = new Student("Luke", 16, true);
+            luke.OutputInfo();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public int age;
+        private bool atSchool;
+
+        public Student(string name, int age, bool atSchool)
+        {
+            this.name = name;
+            this.age = age;
+            this.atSchool = atSchool;
+        }
+
+        public void OutputInfo()
+        {
+            Console.WriteLine($"name: {name}, age: {age}, atSchool: {atSchool}");
+        }
+
+        public void ToggleAtSchool()
+        {
+            atSchool = !atSchool;
         }
     }
 }
